ae0f1fadda81b0329c8f5b1840d67cb412bc2351
wwharton <wwharton@umich.edu>
2021-01-01 12:17:07 -0500

    adds basic forms, gitignore, readme changes for package updates to avoid errors

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, escape, request, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '78dd78dad6a396a8175e8a508ec52100'
 
 posts = [
     {
@@ -26,7 +29,18 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__ == '__main__':
