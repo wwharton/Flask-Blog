5bf5ce1210c5593a78adbbaf968aee47b89d4c40
wwharton <wwharton@umich.edu>
2021-01-05 15:40:43 -0500

    implementes user_auth

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -7,6 +9,12 @@ app.config['SECRET_KEY'] = '78dd78dad6a396a8175e8a508ec52100'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' #this view is my login function in routes
+login_manager.login_message_category = 'info' #info is a bootstrap class
 
 # routes import must follow db init otherwise we fall into an import loop
 from flaskblog import routes
