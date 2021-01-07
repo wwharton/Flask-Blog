a83186dad520ea9a371163c1575ef8cbf0c2cb50
wwharton <wwharton@umich.edu>
2021-01-07 17:44:34 -0500

    adds password reset token / email functionality

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
 
 app = Flask(__name__)
 
@@ -15,6 +17,20 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' #this view is my login function in routes
 login_manager.login_message_category = 'info' #info is a bootstrap class
+
+# Flask Mail config, including windows environment variables
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+
+# OS Environment Variables need to be set on the deployed machine
+# For use in venv, hardcode or activate the values
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+
+
 
 # routes import must follow db init otherwise we fall into an import loop
 from flaskblog import routes
