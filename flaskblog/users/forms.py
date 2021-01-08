f72ef1e8cfc6fe5dfdf8a493392f1652a8a6db41
wwharton <wwharton@umich.edu>
2021-01-08 16:59:08 -0500

    Refactors for Blueprint/Package app factory

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -55,12 +55,6 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('That email is already in use.')
 
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
 class RequestResetForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-
 from flaskblog import app
 
 if __name__ == '__main__':
